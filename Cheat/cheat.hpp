3081fcc0f4d58e029d2b8466d53c6439d8fd3b11
Mzying2001 <Mzying2001@qq.com>
2022-03-01 01:28:27 +0800

    Update cheat.hpp

--- Cheat/cheat.hpp
+++ Cheat/cheat.hpp
@@ -1,23 +1,17 @@
 #pragma once
 
 #include <Windows.h>
+#include <vector>
 
 #define BASE_ADDRESS 0x006A9EC0
+
+using std::vector;
 
 HANDLE GetHandle(HWND hwnd)
 {
     DWORD pid;
     GetWindowThreadProcessId(hwnd, &pid);
     return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-}
-
-LPVOID GetAddress(HANDLE hProc, LPVOID pBase, int *off, int cnt)
-{
-    DWORD buf;
-    ReadProcessMemory(hProc, pBase, &buf, 4, NULL);
-    for (int i = 0; i < cnt; ++i)
-        ReadProcessMemory(hProc, (LPVOID)(buf + off[i]), &buf, 4, NULL);
-    return (LPVOID)buf;
 }
 
 extern "C"
@@ -29,25 +23,43 @@ extern "C"
 
     __declspec(dllexport) void PVZSetSun(HWND hPVZ, int val)
     {
-        int off[] = {0x768, 0x5560};
         HANDLE hProc = GetHandle(hPVZ);
-        LPVOID pSun = GetAddress(hProc, (LPVOID)BASE_ADDRESS, off, 2);
-        WriteProcessMemory(hProc, pSun, (LPVOID)&val, 4, NULL);
+        LPVOID pBase = (LPVOID)BASE_ADDRESS;
+        SIZE_T buf = 0;
+        ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        for (int off : {0x768, 0x5560})
+        {
+            pBase = (LPVOID)(buf + off);
+            ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        }
+        WriteProcessMemory(hProc, (LPVOID)pBase, (LPVOID)&val, 4, NULL);
     }
 
     __declspec(dllexport) void PVZSetCoin(HWND hPVZ, int val)
     {
-        int off[] = {0x82C, 0x28};
         HANDLE hProc = GetHandle(hPVZ);
-        LPVOID pCoin = GetAddress(hProc, (LPVOID)BASE_ADDRESS, off, 2);
-        WriteProcessMemory(hProc, pCoin, (LPVOID)&val, 4, NULL);
+        LPVOID pBase = (LPVOID)BASE_ADDRESS;
+        SIZE_T buf = 0;
+        ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        for (int off : {0x82C, 0x28})
+        {
+            pBase = (LPVOID)(buf + off);
+            ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        }
+        WriteProcessMemory(hProc, (LPVOID)pBase, (LPVOID)&val, 4, NULL);
     }
 
     __declspec(dllexport) void PVZSetAdvProg(HWND hPVZ, int val)
     {
-        int off[] = {0x82C, 0x24};
         HANDLE hProc = GetHandle(hPVZ);
-        LPVOID AdvProg = GetAddress(hProc, (LPVOID)BASE_ADDRESS, off, 2);
-        WriteProcessMemory(hProc, AdvProg, (LPVOID)&val, 4, NULL);
+        LPVOID pBase = (LPVOID)BASE_ADDRESS;
+        SIZE_T buf = 0;
+        ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        for (int off : {0x82C, 0x24})
+        {
+            pBase = (LPVOID)(buf + off);
+            ReadProcessMemory(hProc, pBase, (LPVOID)&buf, 4, NULL);
+        }
+        WriteProcessMemory(hProc, (LPVOID)pBase, (LPVOID)&val, 4, NULL);
     }
 }
